cdfdf800875362e7af38ce0aa204cb761a677c5a
parkerqueen <sobanr4@gmail.com>
2021-06-19 02:09:36 +0500

    Setup the hardhat environment

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+
+const ALCHEMY_API_KEY = 'KEY_HERE';
+const KOVAN_PRIVATE_KEY = 'KEY_HERE';
+const ETHERSCAN_API_KEY = 'KEY_HERE';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: '0.7.3',
+    networks: {
+        kovan: {
+            url: ALCHEMY_API_KEY,
+            accounts: [`0x${KOVAN_PRIVATE_KEY}`],
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+};
